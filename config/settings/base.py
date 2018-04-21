9c16bbd280da08379a12e9b5dec75097aa8bf9a2
dev-smc <smc0210@naver.com>
2018-04-22 00:03:38 +0900

    환경설정 진행

--- config/settings/base.py
+++ config/settings/base.py
@@ -63,7 +63,6 @@ DJANGO_APPS = [
     'django.contrib.admin',
 ]
 THIRD_PARTY_APPS = [
-    'crispy_forms',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -72,6 +71,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     'devgram.users.apps.UsersConfig',
     # Your stuff: custom apps go here
+    'devgram.images.apps.ImagesConfig',
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
